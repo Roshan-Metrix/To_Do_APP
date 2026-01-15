aa22b6b1d161743ba3b62124ef14676c981d2ab1
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-16 01:16:23 +0530

    Initial Commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import useTheme from '@/hooks/useTheme'
 
 const TodosScreen = () => {
+
+ const { toggleDarkMode } = useTheme();
+
   return (
-    <View>
+    <View style={{flex:1,justifyContent:"center",alignItems:"center",gap:4}}>
       <Text>TodosScreen</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text style={{fontSize:34}}>Toggle The Mode</Text>
+      </TouchableOpacity>
     </View>
   )
 }
