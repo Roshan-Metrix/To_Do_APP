9d0535c24827a8a61cdfff41c3d487c7c3c30193
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-16 22:33:20 +0530

    Added components of DangerZone, Preferences , ProgressStat

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import useTheme from "@/hooks/useTheme";
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import ProgressStats from "@/components/ProgressStats";
+import Preferences from "@/components/Preferences";
+import DangerZone from "@/components/DangerZone";
 
 const SettingScreen = () => {
-  return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
-  )
-}
 
-export default SettingScreen
+  const { colors } = useTheme();
+
+  const settingsStyles = createSettingsStyles(colors);
+
+  return (
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingsStyles.container}
+    >
+      <SafeAreaView style={settingsStyles.safeArea}>
+        <View style={settingsStyles.header}>
+          <View style={settingsStyles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingsStyles.iconContainer}
+            >
+              <Ionicons name="settings" size={28} color="#fff" />
+            </LinearGradient>
+            <Text style={settingsStyles.title}>Settings</Text>
+          </View>
+        </View>
+        <ScrollView
+          style={settingsStyles.scrollView}
+          contentContainerStyle={settingsStyles.content}
+          showsHorizontalScrollIndicator={false}>
+        <ProgressStats />
+        <Preferences />
+        <DangerZone />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+export default SettingScreen;
